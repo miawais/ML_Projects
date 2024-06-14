9fe2e889558f533c75a29f69c0b82e52e13b42bc
miawais <awaisbinmukhtar@gmail.com>
2024-06-14 12:51:49 +0500

    model trained and saved, model.pkl file added

--- IrisInsight Project/model.py
+++ IrisInsight Project/model.py
@@ -5,21 +5,21 @@ from sklearn.model_selection import train_test_split
 import pickle
 
 # Load the csv file
-df = pd.read_csv("iris.csv")
+df = pd.read_csv("IrisInsight Project/iris.csv")
 
 print(df.head())
 
-# Select independent and dependent variable
-X = df[["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"]]
-y = df["Class"]
+# Select independent and dependent variables using the correct column names
+X = df[["sepal_length", "sepal_width", "petal_length", "petal_width"]]
+y = df["species"]
 
-# Split the dataset into train and test
+# Split the dataset into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=50)
 
 # Feature scaling
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-X_test= sc.transform(X_test)
+X_test = sc.transform(X_test)
 
 # Instantiate the model
 classifier = RandomForestClassifier()
