eb04d7fbe563f615cc848e56dfa508d0f48146df
miawais <awaisbinmukhtar@gmail.com>
2024-06-14 17:01:41 +0500

    index.html & app.py files updated for background image

--- IrisInsight Project/app.py
+++ IrisInsight Project/app.py
@@ -1,21 +1,16 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
+from flask import Flask, render_template, request, url_for
 
-# Create flask app
-flask_app = Flask(__name__)
-model = pickle.load(open("model.pkl", "rb"))
+app = Flask(__name__)
 
-@flask_app.route("/")
-def Home():
-    return render_template("index.html")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@flask_app.route("/predict", methods = ["POST"])
+@app.route('/predict', methods=['POST'])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+    # Your prediction logic here
+    prediction_text = "Prediction result here"
+    return render_template('your_template.html', prediction_text=prediction_text)
 
-if __name__ == "__main__":
-    flask_app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
